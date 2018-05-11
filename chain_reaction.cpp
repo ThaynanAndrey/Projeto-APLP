558702a07f309ee9028171f349bd57a1b815d273
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2018-05-10 22:47:42 -0300

    Adicionada documentação das funções e refatorado parte do código

--- chain_reaction.cpp
+++ chain_reaction.cpp
@@ -6,56 +6,159 @@
 #define VAZIO 0
 #define J1 1
 #define J2 2
+#define INDICE_PRIMEIRA_LINHA 0
+#define INDICE_PRIMEIRA_COLUNA 0
 
 using namespace std;
 
+/**
+ * Guia utilizado para as posições do tabuleiro, ou seja, letra
+ * A representa coluna 0, letra B coluna 1, e assim por diante.
+ */
 string guias = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+/**
+ * Limpa a tela do terminal para que o tabuleiro seja impresso no final do
+ * terminal, facilitando a interação com o usuário.
+ */
 void limparTela() {
   cout << string( 100, '\n' );
   cout << "\033[1;1H";
 }
 
+/**
+ * Estrutura para definir as posições do tabuleiro.
+ * - cor: cor do jogador;
+ * - x: número da linha no tabuleiro;
+ * - y: número da coluna no tabuleiro;
+ * - pilhaDeBolinhas: quantidade de bolinhas empilhadas nessa posição.
+ */
 struct posicao {
-  int cor, x, y, stack;
+  int cor, x, y, pilhaDeBolinhas;
 };
 
+/**
+ *Limpa o tabuleiro do jogo, deixando todas as posições vazias.
+ */
 void limparTab(posicao tabuleiro[TAM][TAM]) {
   for (int i = 0; i < TAM; i++) {
     for (int j = 0; j < TAM; j++) {
       tabuleiro[i][j].cor = VAZIO;
-      tabuleiro[i][j].stack = VAZIO;
+      tabuleiro[i][j].pilhaDeBolinhas = VAZIO;
       tabuleiro[i][j].x = i;
       tabuleiro[i][j].y = j;
     }
   }
 }
 
-bool isCanto(posicao jogada) {
-  return jogada.x == 0 && jogada.y == 0
-    || jogada.x == 0 && jogada.y == TAM - 1
-    || jogada.x == TAM - 1 && jogada.y == 0
-    || jogada.x == TAM - 1 && jogada.y == TAM - 1;
+/**
+ * Retorna um booleano que indica se a jogada é na quina do tabuleiro.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se está na quina, false caso contrário.
+ */
+bool isQuina(posicao jogada) {
+  const int INDICE_ULTIMA_COLUNA = TAM - 1;
+  const int INDICE_ULTIMA_LINHA = TAM - 1;
+  return jogada.x == INDICE_PRIMEIRA_LINHA && jogada.y == INDICE_PRIMEIRA_COLUNA
+    || jogada.x == INDICE_PRIMEIRA_LINHA && jogada.y == INDICE_ULTIMA_COLUNA
+    || jogada.x == INDICE_ULTIMA_LINHA && jogada.y == INDICE_PRIMEIRA_COLUNA
+    || jogada.x == INDICE_ULTIMA_LINHA && jogada.y == INDICE_ULTIMA_COLUNA;
 }
 
+/**
+ * Retorna um booleano que indica se a jogada foi realizada em uma
+ * das paredes do tabuleiro.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se está em uma das paredes do tabuleiro, false caso contrário.
+ */
 bool isParede(posicao jogada) {
-  return !isCanto(jogada)
-    && (jogada.x == 0
-        || jogada.y == 0
-        || jogada.x == TAM - 1
-        || jogada.y == TAM - 1);
+  const int INDICE_ULTIMA_COLUNA = TAM - 1;
+  const int INDICE_ULTIMA_LINHA = TAM - 1;
+  return !isQuina(jogada)
+    && (jogada.x == INDICE_PRIMEIRA_LINHA
+        || jogada.y == INDICE_PRIMEIRA_COLUNA
+        || jogada.x == INDICE_ULTIMA_LINHA
+        || jogada.y == INDICE_ULTIMA_COLUNA);
 }
 
-bool mayExplode(posicao jogada) {
-  return isCanto(jogada) && jogada.stack > 1
-    || isParede(jogada) && jogada.stack > 2
-    || jogada.stack > 3;
+/**
+ * Retorna um booleano que indica se pode explodir na quina do tabuleiro.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se pode explodir, false caso contrário.
+ */
+bool podeExplodirNaQuina(posicao jogada) {
+  const int QUANTIDADE_MAX_BOLINHAS_QUINA = 1;
+  return isQuina(jogada) && jogada.pilhaDeBolinhas > QUANTIDADE_MAX_BOLINHAS_QUINA;
 }
 
+/**
+ * Retorna um booleano que indica se pode explodir na parede do tabuleiro.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se pode explodir, false caso contrário.
+ */
+bool podeExplodirNaParede(posicao jogada) {
+  const int QUANTIDADE_MAX_BOLINHAS_PAREDE = 2;
+  return isParede(jogada) && jogada.pilhaDeBolinhas > QUANTIDADE_MAX_BOLINHAS_PAREDE;
+}
+
+/**
+ * Retorna um booleano que indica se pode explodir no meio do tabuleiro, ou seja,
+ * não é quina nem parede.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se pode explodir, false caso contrário.
+ */
+bool podeExplodirNoMeio(posicao jogada) {
+  const int QUANTIDADE_MAX_BOLINHAS_MEIO = 3;
+  return jogada.pilhaDeBolinhas > QUANTIDADE_MAX_BOLINHAS_MEIO;
+}
+
+/**
+ * Retorna um valor booleano que indica se a pilha de bolinhas pode explodir,
+ * ou seja, ultrapassou a quantidade limite de bolinhas para a posição.
+ * Explode:
+ *        - Se a jogada é na quina e tem mais de uma bolinha na pilha;
+ *        - Se a jogada é na parede e tem mais de duas bolinhas da pilha;
+ *        - Se a jogada é em outra posição que não é quina nem parede e tem
+ *          mais de três bolinhas na pilha.
+ *
+ * @param {posicao} jogada, Jogada realizada em determinada posição.
+ * @return {bool} True se pode explodir, false caso contrário.
+ */
+bool podeExplodir(posicao jogada) {
+  return podeExplodirNaQuina(jogada)
+        || podeExplodirNaParede(jogada)
+        || podeExplodirNoMeio(jogada);
+}
+
+/**
+ * Retorna um booleano que indica se a coordenada da jogada é válida, o que
+ * implica em respeitar as dimensões máximas do tabuleiro.
+ *
+ * @param {int} x, coordenada x do tabuleiro.
+ * @param {int} y, coordenada y do tabuleiro.
+ * @return {bool} True se é uma coordenada válida, false caso contrário.
+ */
 bool isCoordenadaValida(int x, int y) {
-  return x >= 0 && y >= 0 && x < TAM && y < TAM;
+  const int INDICE_ULTIMA_COLUNA = TAM - 1;
+  const int INDICE_ULTIMA_LINHA = TAM - 1;
+  return x >= INDICE_PRIMEIRA_LINHA
+        && y >= INDICE_PRIMEIRA_COLUNA
+        && x <= INDICE_ULTIMA_LINHA
+        && y <= INDICE_ULTIMA_COLUNA;
 }
 
+/**
+ * Obtém todos os vizinhos de uma posição determinada pela jogada.
+ *
+ * @param {posicao} jogada, jogada realizada em determinada posição.
+ * @param {array} tabuleiro, tabuleiro do jogo.
+ * @return {vector} vetor com os vizinhos.
+ */
 vector<posicao*> getVizinhos(posicao* jogada, posicao tabuleiro[TAM][TAM]) {
   vector<posicao*> saida;
   int x = (*jogada).x;
@@ -75,42 +178,62 @@ vector<posicao*> getVizinhos(posicao* jogada, posicao tabuleiro[TAM][TAM]) {
   return saida;
 }
 
-void printTab(posicao tabuleiro[TAM][TAM]) {
-  limparTela();
-  cout << "------------Tabuleiro--------------" << endl;
-  cout << "   ";
-
-  for (int i = 0; i < TAM; i++) { cout << "  " << guias[i] << " "; }
-  cout << endl;
-  
-  cout << "   ";
-  for (int i = 0; i < TAM; i++) { cout << " __ "; }
-  cout << endl;
-  
+/**
+ * Imprime as posições do tabuleiro do jogo no terminal.
+ * @param {array} tabuleiro, tabuleiro do jogo.
+ */
+void imprimirPosicoesTabuleiro(posicao tabuleiro[TAM][TAM]) {
   for (int i = 0; i < TAM; i++) {
     for (int j = 0; j < TAM; j++) {
       posicao p = tabuleiro[i][j];
-      if (j == 0) { 
-        cout << (i < 9 ? "0" : "") << (i+1) << " |"; 
+      if (j == 0) {
+        cout << (i < 9 ? "0" : "") << (i+1) << " |";
       }
 
       char n[1];
-      sprintf(n, "%d", p.stack);
+      sprintf(n, "%d", p.pilhaDeBolinhas);
 
       cout << " ";
       cout << (p.cor == VAZIO ? " " : p.cor == J1 ? "A" : "B");
-      cout << (p.stack == VAZIO ? " " : n);
+      cout << (p.pilhaDeBolinhas == VAZIO ? " " : n);
       cout << " ";
     }
     cout << "|";
     cout << endl;
   }
-  
   cout << "   ";
-  for (int i = 0; i < TAM; i++) { cout << " __ "; }
+}
+
+/**
+ * Imprime o tabuleiro do jogo no terminal.
+ * @param {array} tabuleiro, tabuleiro do jogo.
+ */
+void imprimirTabuleiro(posicao tabuleiro[TAM][TAM]) {
+  limparTela();
+  cout << "------------Tabuleiro--------------" << endl;
+  cout << "   ";
+
+  for (int i = 0; i < TAM; i++) {
+    cout << "  " << guias[i] << " ";
+  }
+  cout << endl << "   ";
+  for (int i = 0; i < TAM; i++) {
+    cout << " __ ";
+  }
+  cout << endl;
+  imprimirPosicoesTabuleiro(tabuleiro);
+  for (int i = 0; i < TAM; i++) {
+    cout << " __ ";
+  }
   cout << endl;
 }
 
+/**
+ * Realiza a jogada no tabuleiro, resolvendo as explosões que possam acontecer.
+ *
+ * @param {posicao} jogada, jogada realizada em determinada posição.
+ * @param {array} tabuleiro, tabuleiro do jogo.
+ */
 void resolverTabuleiro(posicao* jogada, posicao tabuleiro[TAM][TAM]) {
   deque<posicao*> aProcessar;
   aProcessar.push_back(jogada);
@@ -120,30 +243,33 @@ void resolverTabuleiro(posicao* jogada, posicao tabuleiro[TAM][TAM]) {
     posicao* proximo = aProcessar.front();
     aProcessar.pop_front();
 
-    if (!mayExplode(*proximo)) {
+    if (!podeExplodir(*proximo)) {
       continue;
     }
 
     vector<posicao*> vizinhos = getVizinhos(proximo, tabuleiro);
     for (int i = 0; i < vizinhos.size(); i++) {
       posicao* viz = vizinhos[i];
-      (*viz).stack++;
+      (*viz).pilhaDeBolinhas++;
       (*viz).cor = (*proximo).cor;
-      if (mayExplode(*viz)) {
+      if (podeExplodir(*viz)) {
         aProcessar.push_front(viz);
       } else {
         aProcessar.push_back(viz);
       }
     }
 
-    (*proximo).stack = 0;
+    (*proximo).pilhaDeBolinhas = 0;
     (*proximo).cor = 0;
 
-    printTab(tabuleiro);
+    imprimirTabuleiro(tabuleiro);
     usleep(1000 * 500);
   }
 }
 
+/**
+ * Imprime o menu incial do jogo.
+ */
 int imprimirMenuInicial() {
   cout << "#######################################" << endl;
   cout << "#######################################" << endl;
@@ -163,11 +289,17 @@ int imprimirMenuInicial() {
   return opcao;
 }
 
+/**
+ * Obtém o ganhador do jogo, caso exista.
+ *
+ * @param {array} tabuleiro, Tabuleiro do jogo.
+ * @return {int} inteiro que indica o vencedor do jogo ou 0 se ninguém ganhou.
+ */
 int getGanhador(posicao tabuleiro[TAM][TAM]) {
     int pecasJogadorUm = 0, pecasJogadorDois = 0;
     for (int i = 0; i < TAM; i++) {
       for (int j = 0; j < TAM; j++) {
-        posicao p = tabuleiro[i][j]; 
+        posicao p = tabuleiro[i][j];
         if (p.cor == J1) {
           pecasJogadorUm++;
         } else if (p.cor == J2) {
@@ -178,65 +310,66 @@ int getGanhador(posicao tabuleiro[TAM][TAM]) {
 
     bool ninguemJogou = pecasJogadorDois == 0 && pecasJogadorUm == 0;
     bool doisTemPecas = pecasJogadorDois > 0 && pecasJogadorUm > 0;
-    bool primeiraJogada = pecasJogadorDois == 1 && pecasJogadorUm == 0 
+    bool primeiraJogada = pecasJogadorDois == 1 && pecasJogadorUm == 0
       || pecasJogadorUm == 1 && pecasJogadorDois == 0;
 
     bool ninguemGanhou = primeiraJogada || ninguemJogou || doisTemPecas;
     return ninguemGanhou ? VAZIO : pecasJogadorUm == 0 ? J2 : J1;
 }
 
+/**
+ * Realiza a jogada determinada pelo jogador.
+ *
+ * @param {int} numJogador, Número do jogador responsável pela jogada.
+ * @param {array} tabuleiro, Tabuleiro do jogo.
+ */
 void realizarJogada(int numJogador, posicao tabuleiro[TAM][TAM]) {
     int opcaoColunaJogador, opcaoLinhaJogador;
     char letraLinha;
 
-    printTab(tabuleiro);
+    imprimirTabuleiro(tabuleiro);
 
     cout << endl << "Jogador " << guias[numJogador-1] << endl;
-    cout << "Escolha a posicao: ";
+    cout << "Escolha a posicao (Ex: A1): ";
     cin >> letraLinha >> opcaoColunaJogador;
 
     opcaoLinhaJogador = guias.find(letraLinha, 0);
     opcaoColunaJogador--;
 
     posicao* jogada = & tabuleiro[opcaoColunaJogador][opcaoLinhaJogador];
-    
-    bool casaInvalida = (*jogada).stack > VAZIO && (*jogada).cor != numJogador;
+
+    bool casaInvalida = (*jogada).pilhaDeBolinhas > VAZIO && (*jogada).cor != numJogador;
     if (casaInvalida) {
       realizarJogada(numJogador, tabuleiro);
-      return;
+    } else {
+      if((*jogada).cor == VAZIO) {
+          (*jogada).pilhaDeBolinhas = 1;
+          (*jogada).cor = numJogador;
+      } else if((*jogada).cor == numJogador) {
+          (*jogada).pilhaDeBolinhas++;
+      }
+      resolverTabuleiro(jogada, tabuleiro);
     }
-
-    if((*jogada).cor == VAZIO) {
-        (*jogada).stack = 1;
-        (*jogada).cor = numJogador;
-    } else if((*jogada).cor == numJogador) {
-        (*jogada).stack++;
-    }
-    resolverTabuleiro(jogada, tabuleiro);
 }
 
+/**
+ * Função principal do jogo.
+ */
 int main() {
   int opcao = imprimirMenuInicial();
-  
-  if(opcao == 1) {
+  while(opcao == 1) {
     posicao tabuleiro[TAM][TAM];
     limparTab(tabuleiro);
     int jogadorAtual = J1;
+
     do {
       realizarJogada(jogadorAtual, tabuleiro);
       jogadorAtual = jogadorAtual == J1 ? J2 : J1;
     } while (getGanhador(tabuleiro) == VAZIO);
 
-    cout << endl << "Jogador " << (getGanhador(tabuleiro) == J1 ? "A" : "B") << " venceu!" << endl;
-  } else {
-    cout << "Até a próxima!" << endl;
-  }
+    cout << endl << "Parabéns, Jogador " << (getGanhador(tabuleiro) == J1 ? "A" : "B") << ", você venceu!" << endl << endl;
 
-  // TODO:
-  // 1. Menu para interação no jogo (Quase finalizado)
-  // 2. Definir como será a impressão do tabuleiro (FALTA)
-  // 3. Implementar jogabilidade pela linha de comando (Feito)
-  // 4. Uniformizar código (FALTA)
-  // 5. Documentar, se necessário (FALTA)
-  // 6. Definir se haverá impressão da animação do tabuleiro se resolvendo (FALTA)
+    opcao = imprimirMenuInicial();
+  }
+  cout << "Até a próxima!" << endl;
 }
